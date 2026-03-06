70f5e605c03a020d4ec609b80e9f94e1845734b1
Arash Dehdari <arde1369@yahoo.com>
2026-03-05 21:16:53 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("greetings")
+
+@mcp.tool()
+def greeting(name:str) -> str:
+    """
+        Sends a greeting to the caller.
+    """
+    return f"Hello {name}"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
